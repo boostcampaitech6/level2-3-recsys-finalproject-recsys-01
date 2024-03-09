4404e144547b6b70015265b533ab4f609ca55923
GangBean <xbox2537@gmail.com>
2024-03-09 15:18:12 +0900

    test: 데이터소스 생성 정상 테스트 추가
    #5

--- backend/tests/database/test_database.py
+++ backend/tests/database/test_database.py
@@ -15,3 +15,19 @@ def test_환경변수파일_경로찾기_정상(input, output):
 
     # then
     assert filename == output
+
+@pytest.mark.parametrize("host, port, database", [
+    ('localhost', '27017', 'dev'),
+    ('10.0.6.6', '27017', 'prod'),
+])
+def test_데이터소스_생성_정상(host, port, database):
+    # given
+    config = {
+        'host': host,
+        'port': port,
+        'database': database
+    }
+
+    # when, then
+    with pytest.raises(Exception):
+        DataSource(**config)
