cf83f8e5115703e97b8a4db945799866d5e02f27
GangBean <xbox2537@gmail.com>
2024-03-09 14:49:23 +0900

    refactor: 환경변수파일 경로찾기 테스트 parametrize로 변경
    #5

--- backend/tests/database/test_database.py
+++ backend/tests/database/test_database.py
@@ -1,11 +1,17 @@
+import pytest
+
 from ...app.database.data_source import DataSource, env_file_of
 
-def test_환경변수파일_경로찾기_정상():
+@pytest.mark.parametrize("input, output", [
+    ('dev', 'dev.env'),
+    ('prod', 'prod.env'),
+])
+def test_환경변수파일_경로찾기_정상(input, output):
     # given
-    env = 'dev'
+    env = input
 
     # when
     filename = env_file_of(env).split('/')[-1]
 
     # then
-    assert filename == 'dev.env'
+    assert filename == output
