81e4ec36edf3787aee3e6730d50d2805444e48d3
GangBean <xbox2537@gmail.com>
2024-03-12 20:09:41 +0900

    feat: 유저 service 클래스 구조 작성
    #11

--- backend/app/api/routes/users/service/user_service.py
+++ backend/app/api/routes/users/service/user_service.py
@@ -2,14 +2,23 @@ from ..repository.user_repository import UserRepository
 from ..dto.user_dto import (
     UserSignupDTO, UserLoginDTO,
 )
+from ..controller.request.signup_request import UserFavorRecipesRequest
 
 class UserService:
     def __init__(self, user_repository):
         self.repository: UserRepository = user_repository
 
-    # def sign_up(self, sign_up_request: UserSignupDTO) -> UserSignupDTO:
-    #     return UserSignupDTO(**{
-    #         '_id': '',
-    #         'login_id': '',
-    #         'password': '', 
-    #     })
+    def sign_up(self, sign_up_request: UserSignupDTO) -> UserSignupDTO:
+        return None
+    
+    def is_login_id_usable(self, login_id: str) -> bool:
+        return False
+    
+    def is_nickname_usable(self, nickname: str) -> bool:
+        return False
+    
+    def favor_recipes(self, page_num: int) -> list:
+        return list()
+    
+    def save_favor_recipes(self, login_id: str, request: UserFavorRecipesRequest) -> None:
+        return None
