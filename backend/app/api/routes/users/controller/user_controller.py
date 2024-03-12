04d0e1f66015dbc731e79f58b0204d407c6e8ae6
GangBean <xbox2537@gmail.com>
2024-03-12 20:05:43 +0900

    feat: 유저 API 컨트롤러 작성
    #11

--- backend/app/api/routes/users/controller/user_controller.py
+++ backend/app/api/routes/users/controller/user_controller.py
@@ -1,9 +1,9 @@
 from fastapi import APIRouter, Request, Response, status
 
-from .request.signup_request import SignupRequest, LoginRequest
-from .response.signup_response import SignupResponse
+from .request.signup_request import SignupRequest, LoginRequest, UserFavorRecipesRequest
+from .response.signup_response import SignupResponse, LoginResponse, FavorRecipesResponse
 from ..service.user_service import UserService
-from ..dto.user_dto import UserSignupDTO
+from ..dto.user_dto import UserSignupDTO, UserLoginDTO
 
 
 class UserController:
@@ -16,40 +16,60 @@ class UserController:
                 UserSignupDTO(**signup_request)
             )
         )
+    
+    def is_login_id_usable(self, login_id: str) -> bool:
+        return self.service.is_login_id_usable(login_id)
+    
+    def is_nickname_usable(self, nickname: str) -> bool:
+        return self.service.is_nickname_usable(nickname)
+    
+    def favor_recipes(self, page_num: int) -> list:
+        return self.service.favor_recipes(page_num)
+    
+    def save_favor_recipes(self, login_id: str, request: UserFavorRecipesRequest) -> None:
+        return self.service.save_favor_recipes(login_id, request)
+
 
 user_controller = UserController(UserService())
 router = APIRouter('/api/users')
 
-@router.post('/')
+@router.post()
 async def sign_up(request: SignupRequest) -> Response:
-    # response_body = await user_controller.sign_up(UserSignupDTO(
-    #     login_id=request.login_id,
-    #     password=request.password,
-    #     nickname=request.nickname,
-    #     email=request.email))
-    # return Response(content=SignupResponse(**response_body), status_code=status.HTTP_200_OK)
-    pass
+    response_body = await user_controller.sign_up(UserSignupDTO(
+        login_id=request.login_id,
+        password=request.password,
+        nickname=request.nickname,
+        email=request.email))
+    return Response(content=SignupResponse(**response_body), status_code=status.HTTP_200_OK)
 
 @router.post('/auth')
 async def login(request: LoginRequest) -> Response:
-    pass
+    response_body = await user_controller.login(UserLoginDTO(
+        login_id=request.login_id,
+        password=request.password
+    ))
+    return Response(content=LoginResponse(**response_body), status_code=status.HTTP_200_OK)
 
 # GET /api/users?login_id={login_id}
-@router.get('/')
+@router.get()
 async def is_usable_login_id(login_id: str) -> Response:
-    pass
+    await user_controller.is_login_id_usable(login_id)
+    return Response(status_code=status.HTTP_200_OK)
 
 # GET /api/users?nickname={nickname}
 @router.get('/')
 async def is_usable_nickname(nickname: str) -> Response:
-    pass
+    await user_controller.is_nickname_usable(nickname)
+    return Response(status_code=status.HTTP_200_OK)
 
 # GET /api/foods?page={page_num}
 @router.get('/')
 async def favor_recipes(page_num: int=1) -> Response:
-    pass
+    response_body = await user_controller.favor_recipes(page_num)
+    return Response(content=FavorRecipesResponse(**response_body), status_code=status.HTTP_200_OK)
 
 # POST /api/users/{user_id}/foods
 @router.post('/{user_id}/foods')
-async def save_favor_recipes(user_id: str, UserFavorRequest) -> Response:
-    pass
+async def save_favor_recipes(user_id: str, request: UserFavorRecipesRequest) -> Response:
+    await user_controller.save_favor_recipes(user_id, request)
+    return Response(status_code=status.HTTP_200_OK)
