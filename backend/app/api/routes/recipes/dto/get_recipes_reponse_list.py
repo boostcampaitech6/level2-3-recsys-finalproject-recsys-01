8b030d6f4ca74dab49e6d3683f7940f174d6a2c6
sangwoonoel <sangwoonoel@cau.ac.kr>
2024-03-14 18:04:41 +0900

    fix(dto): API 응답에 유저가 요리한 recipes_id와 next_page_url 추가
    # 21

--- backend/app/api/routes/recipes/dto/get_recipes_reponse_list.py
+++ backend/app/api/routes/recipes/dto/get_recipes_reponse_list.py
@@ -5,21 +5,25 @@ from typing import List
 
 
 class GetRecipesReponseList:
-  response: List[GetRecipesReponse]
- 
-  def __init__(self, recipes: Recipes, ingredients_list: List[Ingredients]):
+  recipe_list: List[GetRecipesReponse]
+  cooked_recipes_id: List[str] 
+  next_page_url: str 
+  
+  def __init__(self, recipes: Recipes, ingredients_list: List[Ingredients], cooked_recipes_id: List[str] = None, next_page_url: str = None):
     super().__init__() 
-    self.response = list()
+    self.recipe_list = list()
     for recipe, ingredients in zip(recipes.get_recipes(), ingredients_list):
       id = recipe.get_id()
       recipe_name = recipe.get_recipe_name()
       recipe_url = recipe.get_recipe_url()
       response_ingredients = self._make_ingredient_dict(ingredients)
       recipe_img_url = recipe.get_recipe_img_url()
-      self.response.append(GetRecipesReponse(
+      self.recipe_list.append(GetRecipesReponse(
         id = id, recipe_name=recipe_name, recipe_url=recipe_url, 
         ingredient=response_ingredients, recipe_img_url=recipe_img_url))
-  
+    self.cooked_recipes_id = cooked_recipes_id
+    self.next_page_url = next_page_url
+ 
  
   def _make_ingredient_dict(self, ingredients: Ingredients):
     result = {ingredient.get_id(): ingredient.get_name() for ingredient in ingredients.get_ingredients()}
