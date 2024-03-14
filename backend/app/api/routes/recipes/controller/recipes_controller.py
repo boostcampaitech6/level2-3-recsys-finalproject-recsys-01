4323e17418c59ae98a5168e67c5315ecbc9099b9
sangwoonoel <sangwoonoel@cau.ac.kr>
2024-03-14 16:51:52 +0900

    fix(controller): 유저가 요리한 레시피 목록 조회 API 반환 값 타입 변경
    # 21

--- backend/app/api/routes/recipes/controller/recipes_controller.py
+++ backend/app/api/routes/recipes/controller/recipes_controller.py
@@ -6,8 +6,7 @@ recipes_router = APIRouter()
 
 @recipes_router.get(
     "/users/{user_id}/recipes/cooked", 
-    response_description="유저가 요리한 레시피 목록",
-    response_model=GetRecipesReponseList
+    response_description="유저가 요리한 레시피 목록"
     )
 def get_user_cooked_recipes_by_page(user_id: str, page_num: int = 0):
     # user_id로 유저가 요리한 레시피 id 리스트 조회
@@ -15,5 +14,5 @@ def get_user_cooked_recipes_by_page(user_id: str, page_num: int = 0):
     # 레시피 id 리스트로 각 레시피의 정보 조회
     recipes = get_recipes_by_recipes_id(user_cooked_recipes_id)
     # 레시피로 Ingredients 리스트 조회
-    ingredients_collection = get_ingredients_list_by_recipes(recipes)
-    return GetRecipesReponseList(recipes, ingredients_collection)
+    ingredients_list = get_ingredients_list_by_recipes(recipes)
+    return GetRecipesReponseList(recipes, ingredients_list)
