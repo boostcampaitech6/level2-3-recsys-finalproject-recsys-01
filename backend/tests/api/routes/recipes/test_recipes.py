ac51e4d58cbecd83f0f728a6533a8612ce0459e3
sangwoonoel <sangwoonoel@cau.ac.kr>
2024-03-14 17:38:56 +0900

    test(test): 재료 조회 테스트 코드 추가
    # 21

--- backend/tests/api/routes/recipes/test_recipes.py
+++ backend/tests/api/routes/recipes/test_recipes.py
@@ -3,6 +3,8 @@ from .....app.api.routes.recipes.repository.recipes_repository import select_use
 from.....app.api.routes.recipes.entity.user import User
 from.....app.api.routes.recipes.entity.recipes import Recipes
 from.....app.api.routes.recipes.entity.recipe import Recipe
+from.....app.api.routes.recipes.entity.ingredients import Ingredients
+from.....app.api.routes.recipes.entity.ingredient import Ingredient
 
 from fastapi.testclient import TestClient
 from .....app.api.routes.recipes.controller.recipes_controller import recipes_router
@@ -54,22 +56,93 @@ def test_select_user_by_user_id(user_id, output):
     assert user.model_dump() == output
 
 
-# feedback list로 레시피를 올바르게 조회하는지 테스트 
+# feedback list로 조회한 레시피 타입 확인
 @pytest.mark.parametrize("recipes_id, output", [
-    (["65f0371e141b7b6fd385c7d8", "65f0371e141b7b6fd385c7d8"], True),
+    (["65f0371e141b7b6fd385c7d8", "65f29506141b7b6fd385c7e9"], True),
 ])
-def test_select_recipes_by_recipes_id(recipes_id, output):
+def test_select_recipes_by_recipes_id_type(recipes_id, output):
     recipes = select_recipes_by_recipes_id(recipes_id)
     assert isinstance(recipes, Recipes) == output
     assert isinstance(recipes.get_recipes()[0], Recipe) == output
+
+
+# feedback list로 레시피를 올바르게 조회하는지 테스트 
+@pytest.mark.parametrize("recipes_id, output", [
+    (["65f0371e141b7b6fd385c7d8", "65f29506141b7b6fd385c7e9"], 
+        {
+            "recipes": [
+                {
+                    "id": "65f0371e141b7b6fd385c7d8",
+                    "food_name": "김치찌개",
+                    "recipe_name": "매콤한 김치찌개",
+                    "ingredient": [
+                        "65f04741141b7b6fd385c7da",
+                        "65f047b9141b7b6fd385c7db"
+                    ],
+                    "time_taken": 30,
+                    "difficulty": "초급",
+                    "recipe_url": "https://www.10000recipe.com/recipe/view.html?seq=6908832&targetList=reviewLists#reviewLists",
+                    "portion": "4인분",
+                    "recipe_img_url": "https://recipe1.ezmember.co.kr/cache/recipe/2019/03/10/ad0e61fd8b4783a926ebccadd0c1b8c11.jpg"
+                },
+                {
+                    "id": "65f29506141b7b6fd385c7e9",
+                    "food_name": "제육볶음",
+                    "recipe_name": "맛있는 제육볶음",
+                    "ingredient": [
+                        "65f29547141b7b6fd385c7eb",
+                        "65f2955e141b7b6fd385c7f1"
+                    ],
+                    "time_taken": 30,
+                    "difficulty": "중급",
+                    "recipe_url": "https://www.10000recipe.com/recipe/view.html?seq=6908832&targetList=reviewLists#reviewLists",
+                    "portion": "2인분",
+                    "recipe_img_url": "https://recipe1.ezmember.co.kr/cache/recipe/2019/03/10/ad0e61fd8b4783a926ebccadd0c1b8c11.jpg"
+                }
+            ]
+        }
+    ),
+])
+def test_select_recipes_by_recipes_id(recipes_id, output):
+    recipes = select_recipes_by_recipes_id(recipes_id)
+    assert recipes.model_dump() == output
     
-    
-# 레시피에서 재료 올바르게 조회하는지 테스트
+
+
+# 재료 타입 확인
 @pytest.mark.parametrize("ingredients_id, output", [
-    (["65f04741141b7b6fd385c7da", "65f047b9141b7b6fd385c7db"], []),
+    (["65f29547141b7b6fd385c7eb", "65f2955e141b7b6fd385c7f1"], True),
+])
+def test_select_ingredients_by_ingredients_id_type(ingredients_id, output):
+    ingredients = select_ingredients_by_ingredients_id(ingredients_id)
+    assert isinstance(ingredients, Ingredients) == output
+    assert isinstance(ingredients.get_ingredients()[0], Ingredient) == output
+    
+
+# 재료를 올바르게 조회하는지 확인
+@pytest.mark.parametrize("ingredients_id, output", [
+    (["65f04741141b7b6fd385c7da", "65f047b9141b7b6fd385c7db"],
+        {
+        "ingredients": [
+            {
+                "id": "65f04741141b7b6fd385c7da",
+                "name": "김치",
+                "price": 5800.0,
+                "price_url": "https://a"
+            },
+            {
+                "id": "65f047b9141b7b6fd385c7db",
+                "name": "삼겹살",
+                "price": 15800.0,
+                "price_url": "https://a"
+            }
+        ]
+        }
+    ),
 ])
 def test_select_ingredients_by_ingredients_id(ingredients_id, output):
-    recipes = select_ingredients_by_ingredients_id(ingredients_id)
+    ingredients = select_ingredients_by_ingredients_id(ingredients_id)
+    assert ingredients.model_dump() == output
     
     
 # API 테스트
