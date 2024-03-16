82437fb6e1d598d8598cf7bb0408c85d08b90e82
Judy <twndus1362@gmail.com>
2024-03-17 01:28:35 +0900

    feat: 유저 피드백 히스토리, 유저 추천 히스토리 페이지 구현 #7 #8

---
+++ frontend/pages/user_history.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+from common import init, page_header 
+from user_history import user_history_page 
+from pages.recommendation import back_to_home_container
+
+if not st.session_state.is_authenticated:
+    page_header()
+    back_to_home_container()
+else:
+    user_history_page()
